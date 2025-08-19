33f329a5ea1f59a77de8174456c8299fe3fbaa23
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-20 00:57:00 +0330

    get user data route

---
+++ src/app/api/profile/[id]/route.ts
@@ -0,0 +1,29 @@
+import { AppError } from "@/utils/AppError";
+import { catchAsync } from "@/utils/catchAsync";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface MyJwtPayload extends JwtPayload {
+  userInfo: {
+    username: string;
+    userId: number;
+  };
+}
+export const GET = catchAsync(
+  async (request: Request, { params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
+
+    const token = request.headers.get("Authorization");
+    console.log(token)
+    if (!token) throw new AppError("access denied", 401);
+    const payload = jwt.verify(
+      token,
+      process.env.ACCESSTOKEN_SECRET!
+    ) as MyJwtPayload;
+
+    const { userId } = payload.userInfo;
+
+    if (userId !== Number(id))
+      return Response.json({ error: "Invalid token" }, { status: 403 });
+    return Response.json({ userId } , {status : 200});
+  }
+);
