64341fee2d37110f040b5fe3031832d5d9d8f054
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-19 17:35:51 +0330

    logout route completed

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,6 +1,23 @@
+import { databaseOperation } from "@/models/dataBase";
+import { cookies } from "next/headers";
+
 import { NextRequest } from "next/server";
 
-export function POST(request: NextRequest): Response {
-  request.cookies.delete("refreshToken");
+export async function POST(request: NextRequest): Promise<Response> {
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+  if (!refreshToken)
+    return Response.json({ message: "No content to send" }, { status: 204 });
+
+  await databaseOperation.deleteToken(refreshToken);
+  const cookieStore = await cookies();
+
+  cookieStore.set("refreshToken", "", {
+    httpOnly: true,
+    path: "/",
+    sameSite: "strict",
+    maxAge: 0,
+  });
+
+  cookieStore.set("userId", "", { path: "/", maxAge: 0 });
   return Response.json({ message: "Logged out" }, { status: 200 });
 }
