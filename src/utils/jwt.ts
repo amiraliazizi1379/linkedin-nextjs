3af1bc85c07509458479589f05c9f1260d2c4e68
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-21 21:43:13 +0330

    id added to payload

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,16 +1,21 @@
 import * as jwt from "jsonwebtoken";
-import { NextResponse } from "next/server";
 
-export  function RefreshToken(email: string) : string  {
-  return jwt.sign({ userInfo: { username: email } }, process.env.REFRESHTOKEN_SECRET!, {
-    expiresIn: "1d",
-  });
+export function RefreshToken(email: string): string {
+  return jwt.sign(
+    { userInfo: { username: email } },
+    process.env.REFRESHTOKEN_SECRET!,
+    {
+      expiresIn: "1d",
+    }
+  );
 }
 
-export  function AccessToken(email: string) : string {
-  return jwt.sign({ userInfo: { username: email } }, process.env.ACCESSTOKEN_SECRET!, {
-    expiresIn: "15m",
-  });
+export function AccessToken(email: string, id: number): string {
+  return jwt.sign(
+    { userInfo: { username: email, userId: id } },
+    process.env.ACCESSTOKEN_SECRET!,
+    {
+      expiresIn: "15m",
+    }
+  );
 }
-// middleware
-/**/
