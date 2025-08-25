7be113d94246dd1c75f79ed4f0089012ebaf14c6
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-25 21:09:51 +0330

    styles updated

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,7 +1,7 @@
 
 export default function Divider(){
     return(
-        <div className="flex items-center mt-6 mx-auto gap-4 justify-center">
+        <div className="flex-center mt-6 mx-auto gap-4 ">
                   <div className="border-t w-full border-gray-300"></div>
                   <span>or</span>
                   <div className="border-t w-full border-gray-300"></div>
