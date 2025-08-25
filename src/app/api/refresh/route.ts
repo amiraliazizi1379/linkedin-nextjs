536d19c2e4b10c9aa2e62a8d5b1e0892185c6544
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-25 21:11:56 +0330

    refresh route updated

--- src/app/api/refresh/route.ts
+++ src/app/api/refresh/route.ts
@@ -1,51 +1,51 @@
 import { catchAsync } from "@/utils/catchAsync";
 import { NextResponse, NextRequest } from "next/server";
-import { AccessToken } from "@/utils/jwt";
-import { jwtVerify } from "jose";
+import { AccessToken, RefreshToken } from "@/utils/jwt";
 import { databaseOperation } from "@/models/dataBase";
-import { JwtPayload } from "jsonwebtoken";
-import { cookies } from "next/headers";
+import * as jwt from "jsonwebtoken";
+import { hashPassword, is_validPassword } from "@/utils/hash";
+import { clearCookie, setCookie } from "@/utils/handlecookie";
 
-interface jwts extends JwtPayload {
+interface jwts extends jwt.JwtPayload {
   userInfo: { username: string; userId: number };
 }
 
 export const POST = catchAsync(
   async (request: NextRequest): Promise<NextResponse> => {
     const token = request.cookies.get("refreshToken")?.value;
-    if (!token)
-      return NextResponse.json(
-        { message: "You need to sign in" },
-        { status: 401 }
-      );
+    const userId = request.cookies.get("userId")?.value;
+    let response = NextResponse.json(
+      { message: "You need to sign in" },
+      { status: 401 }
+    );
+    if (!token || !userId) return response;
 
-    const findUser = await databaseOperation.findToken(token);
+    const findUser = await databaseOperation.findToken(userId);
+    if (!findUser) return clearCookie(response);
 
-    if (!findUser) {
-      const cookieStore = await cookies();
-      cookieStore.set("refreshToken", "", {
-        httpOnly: true,
-        path: "/",
-        sameSite: "strict",
-        maxAge: 0,
-      });
-      return NextResponse.json(
-        { message: "You need to sign in" },
-        { status: 401 }
-      );
-    }
-    const secret = new TextEncoder().encode(process.env.REFRESHTOKEN_SECRET);
-    const { payload } = await jwtVerify<jwts>(token, secret);
-    if (!payload)
-      return NextResponse.json(
-        { message: "You need to sign in" },
-        { status: 401 }
-      );
+    const is_validToken = await is_validPassword(token, findUser.refreshtoken);
+    if (!is_validToken) return clearCookie(response);
+
+    const payload = jwt.verify(token, process.env.REFRESHTOKEN_SECRET!) as jwts;
+
+    if (!payload) return clearCookie(response);
+
+    const newRefreshToken = RefreshToken(findUser.email);
+
+    await databaseOperation.updateToken(
+      await hashPassword(newRefreshToken),
+      findUser.id
+    );
+
     const newAccessToken = AccessToken(payload.userInfo.username, findUser.id);
 
-    return NextResponse.json({
+    let newresponse = NextResponse.json({
       accessToken: newAccessToken,
-      userId: findUser,
+      userId: findUser.id,
     });
+
+    newresponse = setCookie(newresponse, newRefreshToken);
+
+    return newresponse;
   }
 );
