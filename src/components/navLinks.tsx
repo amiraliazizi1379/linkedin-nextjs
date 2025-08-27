48cd89166a6070a2f1b6b1b7995867d41b29bd08
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-27 15:32:42 +0330

    reusable component for links

---
+++ src/components/navLinks.tsx
@@ -0,0 +1,20 @@
+import { ReactElement, ReactNode } from "react";
+import { IconType } from "react-icons";
+
+type props = {
+  // href : string;
+  name: string;
+  component: ReactNode;
+  clasName?: string;
+};
+
+export default function NavLink({ name, component, clasName }: props) {
+  return (
+    <a
+      className={`flex flex-col items-center gap-[4px] cursor-pointer transition-none hover:text-[#171717] ${clasName}`}
+    >
+      <span>{component}</span>
+      <p className="text-[12px] ">{name}</p>
+    </a>
+  );
+}
