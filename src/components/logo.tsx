dd97a1828de1a30b7caac8297583192c88d6d9d7
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-29 19:22:27 +0330

    responsive styles

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Logo({ className, logoComponent }: Props) {
   return (
     <a href="/" className={`${className} flex  items-center text-2xl font-bold`}>
-      <p>Linked</p>
+      <p className="max-[640px]:hidden">Linked</p>
       <FaLinkedin className={`text-3xl ${logoComponent}`} />
     </a>
   );
