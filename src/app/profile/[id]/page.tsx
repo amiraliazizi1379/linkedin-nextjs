297aba76c3feb977a80107f2e6e3502999843ba9
Amirali Azizi <amir7913azizi@gmail.com>
2025-08-29 19:26:45 +0330

    route handling and saving token in client.

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -14,35 +14,43 @@ export default function Profile({
   const { id } = use(params);
 
   const { accesstoken, setAccesstoken } = useUserContext();
-
-  const [data, setData] = useState<{id : number}>();
+  console.log(accesstoken);
+  const [data, setData] = useState<{ id: number }>();
   const router = useRouter();
 
   useEffect(() => {
     const result = async () => {
       const res = await GetUserData(accesstoken, id);
-      
-      const result = await res.json()
-      console.log(result)
-      setData(result);
+      const result = await res.json();
+      if (res.ok && result.accessToken) {
+        setAccesstoken(result.accessToken);
+        const res2 = await GetUserData(accesstoken, id);
+        if (!res2.ok) router.replace("/login");
+      }
+      else{
+        router.push('/login')
+      }
+      //setData(result);
+
       if (!res.ok) router.replace("/login");
     };
 
     result();
   }, [router]);
 
-async function handlelogout(){
-  const res = await fetch('/api/logout' , {
-    method : 'POST' , 
-    headers : {'Content-Type' : 'application/json'} ,
-    credentials : 'include'
-  })
-  if(res.ok) router.push('/login')
-}
+  async function handlelogout() {
+    const res = await fetch("/api/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    if (res.ok) router.push("/login");
+  }
 
-
-  return <div>
-    <button onClick={handlelogout}>log out</button>
-    
-    {data && data.id} welcome to your profile </div>;
+  return (
+    <div>
+      <button onClick={handlelogout}>log out</button>
+      {data && data.id} welcome to your profile{" "}
+    </div>
+  );
 }
