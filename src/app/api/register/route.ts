04f78e2148976e15c5ae08c02f16bf7f22ad2357
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-02 19:25:01 +0330

    cookies are handled on a seprate file

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,8 +1,7 @@
 import { registerUser } from "@/services/registerUser";
 import { AppError } from "@/utils/AppError";
 import { catchAsync } from "@/utils/catchAsync";
-import { strict } from "assert";
-import { cookies } from "next/headers";
+import { setCookie } from "@/utils/handlecookie";
 import { NextResponse } from "next/server";
 
 export const POST = catchAsync(
@@ -11,23 +10,12 @@ export const POST = catchAsync(
 
     const body = await request.json();
     const { accessToken, refreshToken, insertId } = await registerUser(body);
-
-    const cookieStore = await cookies();
-    cookieStore.set("refreshToken", refreshToken, {
-      httpOnly: true,
-      path: "/",
-      //secure : true , //cookie send by https for more secure
-      sameSite: "strict", //the browser can send cookie only from our site
-      maxAge: 60 * 60 * 24 * 1,
-    });
-
-    cookieStore.set('userId' , String(insertId) , {
-      httpOnly : true , 
-      path : '/' , 
-      sameSite : 'lax' , 
-      maxAge : 60 * 60 * 24 * 1
-    })
-
-    return NextResponse.json({ accessToken, insertId }, { status: 200 });
+    let response = NextResponse.json(
+      { accessToken, insertId },
+      { status: 200 }
+    );
+    setCookie(response, refreshToken);
+    setCookie(response, String(insertId), "userId", "/");
+    return response;
   }
 );
