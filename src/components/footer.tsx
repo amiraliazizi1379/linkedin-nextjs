04c3882363e20fbcc1d6f25f2ed1cc40047d550a
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-03 14:20:11 +0330

    clean code.map method replaced

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,33 +1,19 @@
 import Logo from "./logo";
-import "./footer.css";
-
+import { data } from "../data/footerData";
 export default function Footer() {
   return (
-    <footer
-      id="footer-signin"
-      className="flex-center flex-wrap gap-4  text-[12px] mt-6 mb-4"
-    >
-      <Logo className="text-lg" logoComponent="text-xl" />
+    <footer className="flex-center flex-wrap gap-4 text-gray-500 text-[12px] mt-6 mb-4 max-[1023px]:opacity-0">
+      <Logo className="text-lg hover:underline" logoComponent="text-xl" />
 
       <span>© 2025</span>
-      <a href="https://www.linkedin.com/legal/user-agreement?trk=linkedin-tc_auth-button_user-agreement">
-        User Agreement
-      </a>
-      <a href="https://www.linkedin.com/legal/privacy-policy?trk=linkedin-tc_auth-button_privacy-policy">
-        Privacy Policy
-      </a>
-      <a href="https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy">
-        Community Guidelines
-      </a>
-      <a href="https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy">
-        Cookie Policy
-      </a>
-      <a href="https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy">
-        Copyright Policy
-      </a>
-      <a href="https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy">
-        Send Feedback
-      </a>
+
+      {data.map((item, index) => {
+        return (
+          <a className="hover:underline" key={index} href={item.href}>
+            {item.name}
+          </a>
+        );
+      })}
     </footer>
   );
 }
---
+++ src/data/footerData.ts
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    href: "https://www.linkedin.com/legal/user-agreement?trk=linkedin-tc_auth-button_user-agreement",
+    name: "User Agreement",
+  },
+  {
+    href: "https://www.linkedin.com/legal/privacy-policy?trk=linkedin-tc_auth-button_privacy-policy",
+    name: "Privacy Policy",
+  },
+  {
+    href: "https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy",
+    name: "Community Guidelines",
+  },
+  {
+    href: "https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy",
+    name: "Cookie Policy",
+  },
+  {
+    href: "https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy",
+    name: "Copyright Policy",
+  },
+  {
+    href: "https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy",
+    name: "Send Feedback",
+  },
+];
