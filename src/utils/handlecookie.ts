aba29cd031b9152e22746531f99191c60dde68d3
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-05 21:15:19 +0330

    access token are added to handle cookie

--- src/utils/handlecookie.ts
+++ src/utils/handlecookie.ts
@@ -2,30 +2,50 @@ import { NextResponse } from "next/server";
 
 export function setCookie<T>(
   response: NextResponse<T>,
-  token: string,
-  key: string = "refreshToken",
-  path: string = "/api/refresh"
+  refreshToken: string,
+  accessToken: string,
+  id: string
 ): NextResponse<T> {
-  response.cookies.set(key, token, {
+  response.cookies.set("refreshToken", refreshToken, {
     httpOnly: true,
-    path: path,
+    path: "/api",
     sameSite: "strict",
     maxAge: 60 * 60 * 24 * 1,
   });
+  response.cookies.set("accessToken", accessToken, {
+    httpOnly: true,
+    path: "/",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 1,
+  });
+  response.cookies.set("userId", id, {
+    httpOnly: true,
+    path: "/",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 1,
+  });
+
   return response;
 }
 
-export function clearCookie<T>(
-  response: NextResponse<T>,
-  key: string = "refreshToken",
-  path: string = "/api/refresh"
-): NextResponse<T> {
-  response.cookies.set(key, "", {
+export function clearCookie<T>(response: NextResponse<T>): NextResponse<T> {
+  response.cookies.set("refreshToken", "", {
     httpOnly: true,
-    path: path,
+    path: "/api",
+    sameSite: "strict",
+    maxAge: 0,
+  });
+  response.cookies.set("accessToken", "", {
+    httpOnly: true,
+    path: "/",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 1,
+  });
+  response.cookies.set("userId", "", {
+    httpOnly: true,
+    path: "/",
     sameSite: "strict",
     maxAge: 0,
   });
   return response;
 }
-
