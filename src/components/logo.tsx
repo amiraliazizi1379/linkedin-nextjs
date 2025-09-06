40375946800d074c1930695fc4cd55d431690b35
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-06 09:29:48 +0330

    custom styles are improved

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,13 @@ import { FaLinkedin } from "react-icons/fa";
 type Props = {
   className?: string;
   logoComponent?: string;
+  textstyles? : string;
 };
 
-export default function Logo({ className, logoComponent }: Props) {
+export default function Logo({ className, logoComponent , textstyles }: Props) {
   return (
     <a href="/" className={`${className} flex  items-center text-2xl font-bold`}>
-      <p className="max-[640px]:hidden">Linked</p>
+      <p className={`max-[640px]:hidden ${textstyles}`}>Linked</p>
       <FaLinkedin className={`text-3xl ${logoComponent}`} />
     </a>
   );
