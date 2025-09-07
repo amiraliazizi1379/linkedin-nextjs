40b989c2c02dacff863ec3a46cf82356d06f21e2
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-07 22:16:18 +0330

    custom utility class created

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,10 +1,9 @@
-
-export default function Divider(){
-    return(
-        <div className="flex-center mt-6 mx-auto gap-4 ">
-                  <div className="border-t w-full border-gray-300"></div>
-                  <span>or</span>
-                  <div className="border-t w-full border-gray-300"></div>
-                </div>
-    )
+export default function Divider() {
+  return (
+    <div className="flex-center mt-6 mx-auto gap-4 ">
+      <div className="divider"></div>
+      <span>or</span>
+      <div className="divider"></div>
+    </div>
+  );
 }
