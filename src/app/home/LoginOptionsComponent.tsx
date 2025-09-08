2e80462a2131ca305bf2efac57d022ccb6708af9
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-08 13:26:39 +0330

    map method replaced

--- src/app/home/LoginOptionsComponent.tsx
+++ src/app/home/LoginOptionsComponent.tsx
@@ -1,31 +1,28 @@
-import "./home.css";
 import LoginOptions from "../../components/loginOptions";
+import { data } from "./data/noticedata";
 
 export default function LoginOptionsComponent() {
   return (
-    <div
-      id="login-container"
-      className="flex flex-col gap-5  mt-8 w-[28vw]"
-    >
-      <LoginOptions id="google-logo" className="bg-[#0a66c2] text-[#fff]" />
-
-      <a href="/login">Sign in with email</a>
-      <p
-        id="notice-container"
-        className="text-center text-[12px] text-gray-600"
+    <div className="flex flex-col gap-5  mt-8 w-[400px] max-[550px]:w-[90vw] max-[767px]:w-[60vw]">
+      <LoginOptions className="hover:bg-[#0b7ae9] bg-[#0a66c2] text-[#fff] " />
+      <a
+        href="/login"
+        className="flex-center login-button hover:bg-[#F1F1F1] font-bold text-[#6B6B6B] "
       >
+        Sign in with email
+      </a>
+      <p className="text-center w-full max-[767px]:text-left text-[12px] text-gray-600 max-[767px]:text-[16px]">
         By clicking Continue to join or sign in, you agree to LinkedIn’s{" "}
-        <a href="https://www.linkedin.com/legal/user-agreement?trk=linkedin-tc_auth-button_user-agreement">
-          User Agreement
-        </a>{" "}
-        ,{" "}
-        <a href="https://www.linkedin.com/legal/privacy-policy?trk=linkedin-tc_auth-button_privacy-policy">
-          Privacy Policy
-        </a>{" "}
-        and{" "}
-        <a href="https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy">
-          Cookie Policy
-        </a>
+        {data.map((item, index) => {
+          return (
+            <span key={index} className="text-gray-600">
+              <a href={item.link} className="text-[#0a66c2] hover:underline">
+                {item.name}
+              </a>{" "}
+              {index === 2 ? "." : ","}
+            </span>
+          );
+        })}
       </p>
     </div>
   );
---
+++ src/app/home/data/noticedata.ts
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    link: "https://www.linkedin.com/legal/user-agreement?trk=linkedin-tc_auth-button_user-agreement",
+    name: "User Agreement",
+  },
+  {
+    link: "https://www.linkedin.com/legal/privacy-policy?trk=linkedin-tc_auth-button_privacy-policy",
+    name: "Privacy Policy",
+  },
+  {
+    link: "https://www.linkedin.com/legal/cookie-policy?trk=linkedin-tc_auth-button_cookie-policy",
+    name: "Cookie Policy",
+  },
+];
