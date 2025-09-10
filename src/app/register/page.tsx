2a86d44d0253f8cda54be68ab2a14284d8b62e87
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-10 12:40:23 +0330

    styles in tailwind imporved

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,8 +6,8 @@ import { ReactElement } from "react";
 export default function Register() : ReactElement {
   return (
     <main className="min-h-screen">
-      <Logo className="text-[#0a66c2] mt-6 ml-10 text-3xl w-[140px]" />
-      <h1 className="text-center text-3xl px-1.5 mt-4">
+      <Logo className="text-[#0a66c2] mt-6 ml-10  w-[140px]" />
+      <h1 className="text-center text-3xl px-1.5 mt-4 mb-6">
         Make the most of your professional life
       </h1>
       <RegisterForm />
