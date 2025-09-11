90bbe9337d752562bcd97c57f226e327b8af984a
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-11 12:05:48 +0330

    profile navbar data

---
+++ src/app/profile/datas/data.tsx
@@ -0,0 +1,27 @@
+import { MdPeopleAlt } from "react-icons/md";
+import { IoIosBriefcase } from "react-icons/io";
+import { IoHomeSharp, IoNotifications } from "react-icons/io5";
+import { AiFillMessage } from "react-icons/ai";
+
+export const data = [
+  {
+    logo: <IoHomeSharp />,
+    name: "Home",
+  },
+  {
+    logo: <MdPeopleAlt />,
+    name: "My Network",
+  },
+  {
+    logo: <IoIosBriefcase />,
+    name: "Jobs",
+  },
+  {
+    logo: <AiFillMessage />,
+    name: "Messaging",
+  },
+  {
+    logo: <IoNotifications />,
+    name: "Notifications",
+  },
+];
