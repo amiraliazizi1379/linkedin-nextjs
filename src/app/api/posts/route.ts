486ac7b2e5d8fe2597a5056c7dc0cdecfb2c5cb6
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-15 20:49:01 +0330

    post api route added to project

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,30 @@
+import { auth } from "@/libs/accessTokenVerify";
+import { databaseOperation } from "@/models/dataBase";
+import { catchAsync } from "@/utils/catchAsync";
+import { writeFile } from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = auth(
+  async (request: NextRequest, userId: number): Promise<NextResponse> => {
+    const formData = await request.formData();
+    const text = formData.get("text") as string;
+    const image = formData.get("img") as File | null;
+
+    let imageUrl = null;
+
+    if (image) {
+      const bytes = await image.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const path = `C:/Users/AMIR/Desktop/linked-in-nextjs-typescript/public/postimg/${image.name}`;
+      await writeFile(path, buffer);
+      imageUrl = `/postimg/${image.name}`;
+    }
+
+    await databaseOperation.addPost(userId, text, imageUrl);
+
+    return NextResponse.json(
+      { message: "post successfully added" },
+      { status: 200 }
+    );
+  }
+);
