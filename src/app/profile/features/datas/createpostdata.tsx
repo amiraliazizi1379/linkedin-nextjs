2535b8550ed4fc12a6c09bca5d2b94aa869e7934
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-20 10:47:35 +0330

    create post buttons and icons data

---
+++ src/app/profile/features/datas/createpostdata.tsx
@@ -0,0 +1,21 @@
+import { MdSmartDisplay } from "react-icons/md";
+import { HiOutlinePhoto } from "react-icons/hi2";
+import { PiTextAlignLeft } from "react-icons/pi";
+
+export const createPostData = [
+  {
+    name: "Video",
+    logo: <MdSmartDisplay />,
+    color : 'text-green-700'
+  },
+  {
+    name: "Photo",
+    logo: <HiOutlinePhoto />,
+    color : 'text-blue-700'
+  },
+  {
+    name: "Write a article",
+    logo: <PiTextAlignLeft />,
+    color : 'text-[#e67e22]'
+  },
+];
