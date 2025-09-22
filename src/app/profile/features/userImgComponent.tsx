481a6a8c3641c776747e33621b1ddb0224312c3e
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-23 00:11:19 +0330

    image profile component added

---
+++ src/app/profile/features/userImgComponent.tsx
@@ -0,0 +1,9 @@
+export default function UserImageComponent({ style }: { style: string }) {
+  return (
+    <div
+      className={`${style}  rounded-full bg-[#c0392b] flex-center text-[#fff]`}
+    >
+      A
+    </div>
+  );
+}
