8520ceeb0c9cca7b5ef3098f4eff90ee5d34d705
Amirali Azizi <amir7913azizi@gmail.com>
2025-09-22 23:25:26 +0330

    auth middaleware created

--- src/utils/getNewAccessToken.ts
+++ src/utils/getNewAccessToken.ts
@@ -1,32 +1,38 @@
+import { redirect } from "next/navigation";
+
 export async function GetNewAccessToken(
   url: string,
-  options: { method: string; body?: FormData }
-): Promise<Response> {
+  options: { method: string; body?: FormData | string }
+) {
   try {
     const res = await fetch(url, {
       method: options.method,
       body: options.body,
+      credentials: "include",
     });
+
+    if (res.ok) return res;
     const result = await res.json();
     console.log(result);
     if (res.status === 401 && result.message === "Token expired") {
       const requestToken = await fetch("http://localhost:3000/api/refresh", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json ",
+        },
         credentials: "include",
       });
+      if (!requestToken.ok) redirect("/login");
+
       if (requestToken.ok) {
-        const res = await fetch(url, {
+        const res2 = await fetch(url, {
           method: options.method,
-
           body: options.body,
+          credentials: "include",
         });
-        return res;
+        if (res2.ok) return res2;
       }
-      return requestToken;
     }
-
-    return res;
   } catch (err) {
     throw err;
   }
