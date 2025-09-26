49aae278592d14cd9583200736a186d22f567592
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-09-26 18:30:30 +0330

    middleware imporoved and work correctly

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,35 @@
 import { NextResponse, NextRequest } from "next/server";
-import { clearCookie } from "./utils/handlecookie";
+import { jwtVerify } from "jose";
+
+type jwtpayload = {
+  userInfo: {
+    userId: number;
+  };
+};
 
 export const middleware = async (
   request: NextRequest
 ): Promise<NextResponse> => {
   try {
-    const accessToken = request.cookies.get("accessToken")?.value;
-    const { pathname } = request.nextUrl;
+    const refreshToken = request.cookies.get("refreshToken")?.value;
 
-    if (pathname.startsWith("/api") || pathname.startsWith("/profile")) {
-      if (!accessToken) {
-        let res = NextResponse.redirect(new URL("/login", request.nextUrl));
-        clearCookie(res);
-        return res;
-      }
+    if (refreshToken) {
+      const secret = new TextEncoder().encode(process.env.REFRESHTOKEN_SECRET);
+      const { payload } = await jwtVerify<jwtpayload>(refreshToken, secret);
+      console.log(payload);
+      return NextResponse.redirect(
+        new URL(`/profile/${payload.userInfo.userId}`, request.nextUrl)
+      );
     }
-
     return NextResponse.next();
   } catch {
     return NextResponse.json(
-      { message: "something went wron in middleware" },
+      { message: "something went wrong in middleware" },
       { status: 401 }
     );
   }
 };
 
 export const config = {
-  matcher: ["/api/posts", "/profile/:path*", "/login", "/register", "/"],
+  matcher: ["/login", "/register", "/"],
 };
