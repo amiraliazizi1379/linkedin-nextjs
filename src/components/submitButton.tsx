1b442730e13b0a1ace81f5e97c893c3695cbb0fc
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-09-29 23:08:28 +0330

    loading component added to custom submit button

--- src/components/submitButton.tsx
+++ src/components/submitButton.tsx
@@ -1,15 +1,18 @@
+import { BeatLoader } from "react-spinners";
+
 type propType = {
   name: string;
   className?: string;
+  loading: boolean;
 };
 
-export default function SubmitButton({ name, className }: propType) {
+export default function SubmitButton({ name, className , loading }: propType) {
   return (
     <button
       type="submit"
       className={`${className} bg-[#0a66c2] hover:bg-[#06417c] w-full  rounded-full  text-white cursor-pointer`}
     >
-      {name}
+      {loading ? <BeatLoader color="white" size={10} /> : name}
     </button>
   );
 }
