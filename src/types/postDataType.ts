3d720cc7b2ea5baa22db926d75888d3cd986e06d
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-07 14:18:57 +0330

    post data type file created

---
+++ src/types/postDataType.ts
@@ -0,0 +1,12 @@
+import { commentDataType } from "./commentDataType";
+
+export type postdatas = {
+  post_id: number;
+  user_id: number;
+  content: string;
+  image_url: string;
+  liked?: boolean;
+  is_following: boolean;
+  comment: boolean;
+  commentData: commentDataType[];
+};
