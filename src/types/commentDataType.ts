6959544b1373c6439d232bbb108c967bcdc67559
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-13 13:49:00 +0330

    comment data type is created

--- src/types/commentDataType.ts
+++ src/types/commentDataType.ts
@@ -1,10 +1,13 @@
 import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
 
 export type commentDataType = {
-  id: number;
+  comment_id: number;
   user_id: number;
   post_id: number;
   content: string;
   created_at: Timestamp;
   image_url: string;
+  name: string;
+  image: string;
+  email: string;
 };
