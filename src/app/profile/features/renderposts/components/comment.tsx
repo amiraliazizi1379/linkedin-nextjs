d57f76b9acfd7be1e82ba9f2504f3b1c063f59f0
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-14 19:13:47 +0330

    states are added to redux

--- src/app/profile/features/renderposts/components/comment.tsx
+++ src/app/profile/features/renderposts/components/comment.tsx
@@ -1,36 +1,74 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 import { AddImageButton } from "../../createPost/components/addImgBt";
+import UserImageComponent from "../../components/userImgComponent";
+import { CustomActionBtn } from "../../createPost/components/customActionBt";
+import { PostImageUploader } from "../../createPost/components/preview-image";
+import { handleComment } from "../services/handleComments";
+import {
+  RootState,
+  setPostBt,
+  setCommentText,
+  setCommentImgSrc,
+} from "@/app/redux/store";
+import { useDispatch, useSelector } from "react-redux";
 import { useCreatePost } from "../../createPost/hooks/useCreatePost";
-import UserImageComponent from "../../userImgComponent";
-import { PostAction } from "../../createPost/components/postActions";
-import { PostImageUploader } from "../../createPost/components/postImageUploader";
 
-export function CommentComponent() {
-  const state = useCreatePost();
-  const [comment, setCommetn] = useState("");
+export function CommentComponent({ postId }: { postId: number }) {
+  const dispatch = useDispatch();
+  const { userData, commentImgSrc, commentText } = useSelector(
+    (state: RootState) => state.app
+  );
+  const { name, email, image } = userData;
+  const { postImgFile } = useCreatePost();
+  const ctextRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (ctextRef.current) {
+      ctextRef.current.style.height = "auto"; // reset
+      ctextRef.current.style.height = ctextRef.current.scrollHeight + "px"; // تغییر ارتفاع بر اساس متن
+    }
+  }, [commentText]);
+
   return (
-    <section className="flex-center gap-4 mt-4 mb-4 relative">
-      <UserImageComponent style="w-[30px] h-[30px] text-md" />
+    <section className={`flex justify-center gap-2 mt-4 mb-4 relative`}>
+      <UserImageComponent
+        style="w-[40px] h-[40px] text-md"
+        name={name}
+        image={image}
+        email={email}
+      />
       <textarea
-        onChange={(e) => setCommetn(e.target.value)}
+        onChange={(e) => {
+          dispatch(setPostBt(true));
+          dispatch(setCommentText(e.target.value));
+        }}
         placeholder="Add a comment"
-        className={`rounded-full z-0 w-[80%] border-1 h-10 resize-none pt-1.5 px-4 border-gray-400 outline-gray-400 ${
-          comment && "h-[80px] rounded-xl"
-        } ${state.srcImg && 'h-[200px] rounded-xl'}`}
+        ref={ctextRef}
+        value={commentText}
+        className={`rounded-full z-0 w-[85%] border-1 ${
+          !(commentText || commentImgSrc) && "h-10"
+        }  overflow-hidden resize-none pt-1.5 px-4 border-gray-400 outline-gray-400 ${
+          commentText && "min-h-[80px] rounded-xl pb-14"
+        } ${commentImgSrc && "min-h-[200px] rounded-xl pb-[220px]"}`}
       ></textarea>
-      <PostImageUploader {...state} classname="absolute w-[35%] rounded-lg top-8 left-24 "/>
-      <AddImageButton
-        {...state}
-        classname={`absolute right-9.5 hover:bg-gray-200 rounded-full w-[35px] z-5 p-1.5 flex-center ${
-          comment && "right-[450px] top-10.5"
+      <PostImageUploader
+        classname={`absolute w-[35%] rounded-lg bottom-4 left-24 ${
+          !commentImgSrc && "hidden"
         }`}
       />
-      {(comment || state.srcImg) && (
-        <PostAction
-          {...state}
-          postBt={true}
+      <AddImageButton
+        classname={`absolute right-6 text-2xl hover:bg-gray-200 rounded-full w-[35px]  p-1.5 flex-center ${
+          commentText && "left-[5rem] z-5 bottom-2"
+        } `}
+        setState={setCommentImgSrc}
+      />
+      {(commentText || commentImgSrc) && (
+        <CustomActionBtn
+          onclick={(e) => handleComment(e, postId, postImgFile)}
           name="Comment"
-          classname={`absolute border-none right-12 top-6.5 ${state.srcImg && 'bottom-0 top-20'}`}
+          classname={`absolute border-none bottom-4 right-12`}
+          text={commentText}
+          ImgSrc={commentImgSrc}
         />
       )}
     </section>
