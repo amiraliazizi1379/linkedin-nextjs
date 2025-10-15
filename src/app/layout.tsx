7692aceb250c078dcd9019fe30fffe24fce864a9
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-15 23:37:26 +0330

    redux porvider replaced context porvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AppProvider from "../context/useContext";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +15,6 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LinkedIn",
- 
 };
 
 export default function RootLayout({
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <AppProvider>
-        {children}
-        </AppProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
