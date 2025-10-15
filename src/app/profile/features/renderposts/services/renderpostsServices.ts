43b27b7a2145ad661ce581e7693f775ae079edf1
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-15 23:36:08 +0330

    redux replaced context

--- src/app/profile/features/renderposts/services/renderpostsServices.ts
+++ src/app/profile/features/renderposts/services/renderpostsServices.ts
@@ -1,17 +1,11 @@
+import { setLoading, setPostData } from "@/app/redux/store";
 import { GetNewAccessToken } from "@/utils/getNewAccessToken";
-import { useEffect, useState } from "react";
-
-type datas = {
-  post_id: number;
-  user_id: number;
-  content: string;
-  image_url: string;
-  liked?: boolean;
-  is_following: boolean;
-};
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 export function RenderPostsServices() {
-  const [data, setData] = useState<datas[]>([]);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const res = async () => {
       const req = await GetNewAccessToken(
@@ -22,74 +16,9 @@ export function RenderPostsServices() {
       );
       if (req) {
         const result = await req.json();
-        setData(result.postdata);
+        dispatch(setPostData(result.postdata));
       }
     };
     res();
   }, []);
-
-  async function handleLikes(id: number, liked: boolean | undefined) {
-    setData((pre) =>
-      pre.map((post) =>
-        post.post_id === id ? { ...post, liked: !post.liked } : post
-      )
-    );
-
-    try {
-      if (!liked) {
-        const res = await GetNewAccessToken("/api/registerlike", {
-          method: "POST",
-          body: JSON.stringify(id),
-        });
-      } else {
-        const res2 = await GetNewAccessToken("/api/registerlike", {
-          method: "DELETE",
-          body: JSON.stringify(id),
-        });
-      }
-    } catch {
-      setData((pre) =>
-        pre.map((post) =>
-          post.post_id === id ? { ...post, liked: !post.liked } : post
-        )
-      );
-    }
-  }
-
-  async function handleFollow(id: number, is_following: boolean) {
-    setData((pre) =>
-      pre.map((post) =>
-        post.user_id === id
-          ? { ...post, is_following: !post.is_following }
-          : post
-      )
-    );
-
-    try {
-      if (!is_following) {
-        const res = await GetNewAccessToken("/api/registerfollow", {
-          method: "POST",
-          body: JSON.stringify(id),
-        });
-      } else {
-        const res2 = await GetNewAccessToken("/api/registerfollow", {
-          method: "DELETE",
-          body: JSON.stringify(id),
-        });
-      }
-    } catch {
-      setData((pre) =>
-        pre.map((post) =>
-          post.user_id === id
-            ? { ...post, is_following: !post.is_following }
-            : post
-        )
-      );
-    }
-  }
-  return {
-    handleLikes,
-    handleFollow,
-    data,
-  };
 }
