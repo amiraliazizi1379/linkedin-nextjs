c224a2b21f4353bca04acb265fdba58e22377c3b
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-16 14:46:02 +0330

    redux stated and reducers are replaced parameters

--- src/app/profile/features/renderposts/services/getcomment.ts
+++ src/app/profile/features/renderposts/services/getcomment.ts
@@ -1,29 +1,35 @@
-import { contextType } from "@/context/useContext";
+import {
+  setCommentImgSrc,
+  setCommentText,
+  setPostData,
+  store,
+} from "@/app/redux/store";
+
 import { GetNewAccessToken } from "@/utils/getNewAccessToken";
 
-export async function GetComments(
-  id: number,
-  setPostData: contextType["setPostData"],
-  status: boolean
-) {
-  setPostData((pre) =>
-    pre.map((post) =>
-      post.post_id === id ? { ...post, comment: status } : post
-    )
+export async function GetComments(id: number) {
+  const postData = store.getState().app.postData;
+
+  let updatedData = postData.map((post) =>
+    post.comment === true ? { ...post, comment: false } : post
   );
+
+  updatedData = updatedData.map((post) =>
+    post.post_id === id ? { ...post, comment: true } : post
+  );
+  store.dispatch(setCommentText(""));
+  store.dispatch(setCommentImgSrc(""));
+  store.dispatch(setPostData(updatedData));
   try {
     const res = await GetNewAccessToken("/api/getcomments", {
       method: "GET",
       headers: { postId: id },
     });
     const result = await res?.json();
-    setPostData((pre) =>
-      pre.map((post) =>
-        post.post_id === id
-          ? { ...post, commentData: result.commentData }
-          : post
-      )
+    updatedData = updatedData.map((post) =>
+      post.post_id === id ? { ...post, commentData: result.commentData } : post
     );
+    store.dispatch(setPostData(updatedData));
   } catch (err) {
     console.log(err);
   }
