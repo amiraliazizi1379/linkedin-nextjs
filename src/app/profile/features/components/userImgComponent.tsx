5583b5a1882ca1318c84621ddaa448b03d0d56f1
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-19 14:22:22 +0330

    render user image ,  redux states and user info props added to component

--- src/app/profile/features/components/userImgComponent.tsx
+++ src/app/profile/features/components/userImgComponent.tsx
@@ -1,9 +1,47 @@
-export default function UserImageComponent({ style }: { style: string }) {
+"use client";
+
+import { setLargImg, setFullScreenSrc } from "@/app/redux/store";
+import { useDispatch } from "react-redux";
+
+export default function UserImageComponent({
+  style,
+  image,
+  name,
+  email,
+  dontShowLarg 
+}: {
+  style: string;
+  image: string;
+  name: string;
+  email: string;
+  dontShowLarg? : boolean
+}) {
+  const dispatch = useDispatch();
+  let content = "";
+  if (name) {
+    content = name.split("")[0].toUpperCase();
+  } else if (email) {
+    content = email.split("")[0].toUpperCase();
+  }
+
+  if (image) {
+    return (
+      <img
+        onClick={() => {
+          if(!dontShowLarg)
+          dispatch(setLargImg(true));
+          dispatch(setFullScreenSrc(image));
+        }}
+        src={image}
+        className={`object-cover rounded-full cursor-pointer flex-center ${style} `}
+      />
+    );
+  }
   return (
     <div
       className={`${style}  rounded-full bg-[#c0392b] flex-center text-[#fff]`}
     >
-      A
+      {content}
     </div>
   );
 }
