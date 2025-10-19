472e13b2ad6b2bf4cb3d2fc1af7ca771e7084fb6
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-19 14:17:59 +0330

    redux states replaces context

--- src/app/profile/features/createPost/components/customActionBt.tsx
+++ src/app/profile/features/createPost/components/customActionBt.tsx
@@ -1,33 +1,33 @@
+import { RootState } from "@/app/redux/store";
+import { useSelector } from "react-redux";
 import { ClipLoader } from "react-spinners";
 
-
 type props = {
-  postBt: boolean;
-  srcImg: string;
   classname?: string;
   name?: string;
-  loading: boolean;
+  ImgSrc: string;
+  text?: string;
   onclick: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void>;
 };
 
-export function PostAction({
-  postBt,
-  srcImg,
+export function CustomActionBtn({
   classname,
   name = "Post",
-  loading,
+  ImgSrc,
+  text,
   onclick,
 }: props) {
+  const { loading } = useSelector((state: RootState) => state.app);
   return (
     <div
       className={`flex items-center ${classname} gap-4 justify-end border-t-1 border-gray-300 w-full pt-4 ${
-        srcImg && "mt-10"
+        ImgSrc && "mt-10"
       }`}
     >
       <button
         onClick={(e) => onclick(e)}
         className={`${
-          postBt || srcImg
+          text || ImgSrc
             ? "bg-[#0a66c2] text-[#fff] hover:bg-[#0f467c] cursor-pointer"
             : "bg-gray-200 text-gray-400  cursor-not-allowed"
         } rounded-full   py-1 px-4  text-sm font-semibold flex-center`}
