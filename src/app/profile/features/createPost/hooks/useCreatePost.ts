d76ed612d9ba9e778d2a313db3a6c016ab62637f
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-26 14:16:24 +0330

    states move to store

--- src/app/profile/features/createPost/hooks/useCreatePost.ts
+++ src/app/profile/features/createPost/hooks/useCreatePost.ts
@@ -1,65 +1,14 @@
-import { ChangeEvent, useRef, useState } from "react";
-import { GetNewAccessToken } from "@/utils/getNewAccessToken";
-import { useUserContext } from "@/context/useContext";
-import { number } from "joi";
+import { useRef } from "react";
 
 export function useCreatePost() {
-  const { createPost, setCreatePost } = useUserContext();
-  const [postBt, setPostBt] = useState(false);
-  const [postImg, setPostImg] = useState<File | null>(null);
-  const [postText, setPostText] = useState("");
-  const [srcImg, setSrcImg] = useState("");
-  const [heightImage , setHeightImage] = useState(number);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+
   const handleImgBt = () => {
     fileInputRef.current?.click();
   };
 
-  function handleImgInput(e: ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-
-    if (file) {
-      setPostImg(file);
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setSrcImg(reader.result as string);
-      };
-      
-      reader.readAsDataURL(file);
-    }
-  }
-
-  async function handlePost(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    const formData = new FormData();
-    if (postImg) formData.append("img", postImg);
-    formData.append("text", postText);
-
-    try {
-      const res = await GetNewAccessToken("/api/posts", {
-        method: "POST",
-        body: formData,
-      });
-
-      setCreatePost(false);
-    } catch (err) {
-      console.log(err);
-    }
-  }
   return {
-    postBt,
-    setPostBt,
-    postImg,
-    setPostImg,
-    postText,
-    setPostText,
-    srcImg,
-    setSrcImg,
-    heightImage,
-    setHeightImage,
     fileInputRef,
     handleImgBt,
-    handleImgInput,
-    handlePost
   };
 }
