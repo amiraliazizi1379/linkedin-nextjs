bdd3342ae3df54583fe9f333ff7524ae0c3f58a1
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-26 14:17:15 +0330

    styles improved

--- src/app/profile/features/components/navbar.tsx
+++ src/app/profile/features/components/navbar.tsx
@@ -15,8 +15,8 @@ export default function ProfileNavBar() {
   const { name, email, image } = userData;
 
   return (
-    <nav className="flex items-center justify-around p-2 bg-[#fff]">
-      <div className="flex items-center w-[500px] space-x-2 relative">
+    <nav className="flex items-center justify-around pt-1 bg-[#fff]">
+      <div className="flex items-center mb-1 w-[500px] space-x-2 relative">
         <Logo
           className="text-[#0a66c2]"
           logoComponent="text-4xl"
@@ -30,11 +30,11 @@ export default function ProfileNavBar() {
           className=" py-1 px-10 profile-inputs"
         />
       </div>
-      <article className="flex items-center gap-8">
+      <article className="flex items-center h-[3.2rem]">
         <Options datas={data} items="text-2xl" />
         <button
           onClick={() => dispatch(setPopup(!popup))}
-          className="cursor-pointer flex-center flex-col hover:text-[#171717] text-[12px]"
+          className="cursor-pointer flex-center flex-col hover:text-[#171717] text-[12px] ml-2"
         >
           <UserImageComponent
             style="w-[25px] h-[25px] text-[11px]"
