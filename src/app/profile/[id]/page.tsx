b5d1fa61e3e3c6e3f65439411eef2b5d2fb073de
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-10-26 14:22:02 +0330

    redux dispatch method replaced the context. edit profile and render post component are added

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,42 +1,64 @@
 "use client";
 
 import { GetNewAccessToken } from "../../../utils/getNewAccessToken";
-import ProfileNavBar from "../features/navbar";
+import ProfileNavBar from "../features/components/navbar";
 import CreatePost from "../features/createPost/components/createPost";
-import PopOp from "../features/pop-up-user-nav";
 import CreatePostComponent from "../features/createPost/components/createPostComponent";
 import RenderPosts from "../features/renderposts/components/renderPosts";
-import { useEffect } from "react";
-import { useUserContext } from "@/context/useContext";
+import React, { useEffect } from "react";
+import PopOp from "../features/components/pop-up-user-nav";
+import { EditProfile } from "../features/components/editProfileCom/editProfile";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  RootState,
+  setLoading,
+  setPopup,
+  setUserData,
+} from "../../redux/store";
+import Loading from "./loading";
 
-export default function Profile() {
-  const { popup, setPopup } = useUserContext();
+export default function Profile({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = React.use(params);
+  const dispatch = useDispatch();
+  const { popup, loading } = useSelector((state: RootState) => state.app);
 
   useEffect(() => {
     const res = async () => {
+      dispatch(setLoading(true));
       try {
         const res = await GetNewAccessToken("http://localhost:3000/api/user", {
-          method: "GET",
+          method: "POST",
+          body: id,
         });
+        const result = await res?.json();
+        if (res?.status === 403) window.location.href = `/profile/${result.id}`;
+        dispatch(setUserData(result.userData));
       } catch (err) {
         console.log(err);
       }
     };
     res();
   }, []);
-
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <main
       onClick={() => {
-        if (popup) setPopup(false);
+        if (popup) dispatch(setPopup(false));
       }}
       className="bg-gray-100  h-screen overflow-y-auto"
     >
       <ProfileNavBar />
       {popup && <PopOp />}
+      <EditProfile />
       <CreatePost />
       <CreatePostComponent />
-      <RenderPosts />
+      <RenderPosts userId={id} />
     </main>
   );
 }
