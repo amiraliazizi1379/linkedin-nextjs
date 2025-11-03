02831e93f8a48df4fbe3974cecc128d780cfd60d
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-03 22:11:04 +0330

    login route handler is created

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,37 @@
+import { databaseOperation } from "@/models/dataBase";
+import { catchAsync } from "@/utils/catchAsync";
+import { setCookie } from "@/utils/handlecookie";
+import { is_validPassword } from "@/utils/hash";
+import { Validator } from "@/utils/joiValidator";
+import { AccessToken, RefreshToken } from "@/utils/jwt";
+import { NextResponse } from "next/server";
+
+export const POST = catchAsync(async (req: Request) => {
+  const data = await req.json();
+
+  await Validator(data);
+
+  const { email, password } = data;
+
+  const emailExist = await databaseOperation.findEmail(email);
+  if (!emailExist)
+    return NextResponse.json(
+      { message: "email or password is invalid" },
+      { status: 400 }
+    );
+
+  const validPassword = await is_validPassword(password, emailExist.password);
+  if (!validPassword)
+    return NextResponse.json(
+      { message: "email or password  is invalid" },
+      { status: 400 }
+    );
+
+  const refreshToken = RefreshToken(emailExist.id);
+  await databaseOperation.updateToken(refreshToken, emailExist.id);
+
+  const accessToken = AccessToken(emailExist.email, emailExist.id);
+  let res = NextResponse.json({ id: emailExist.id }, { status: 200 });
+  setCookie(res, refreshToken, accessToken);
+  return res;
+});
