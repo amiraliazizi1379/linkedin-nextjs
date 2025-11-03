ab0b64264ceeed20d82a5e4dd9712ebf27241ec6
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-03 22:04:49 +0330

    name  is changed

---
+++ src/app/login/services/handleLogin.ts
@@ -0,0 +1,19 @@
+import { setLoginLoading, store } from "@/redux/store";
+import { loginType } from "@/validation/loginSchema";
+import { redirect } from "next/navigation";
+
+export async function LoginOnSubmit(data: loginType) {
+  try {
+    store.dispatch(setLoginLoading(true));
+    const res = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    const result = await res.json();
+    console.log(result);
+    if (res.status === 200) window.location.href = `/profile/${result.id}`;
+    store.dispatch(setLoginLoading(false));
+  } catch (err) {
+    console.log(err);
+  }
+}
