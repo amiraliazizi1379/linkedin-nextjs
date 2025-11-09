d5206896664c0e1ba42be5d62c2124c379e98937
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-09 15:50:00 +0330

    create avatar color function is created

--- src/utils/createAvatarColor.ts
+++ src/utils/createAvatarColor.ts
@@ -1,14 +1,3 @@
-const COLORS = [
-  "#E17076", // Red/Pink
-  "#E6954E", // Orange
-  "#EBC86E", // Yellow
-  "#6CC070", // Green
-  "#65AADD", // Light Blue
-  "#A695E7", // Purple
-  "#EE7AAE", // Pink Purple
-  "#8EC1A0", // Mint
-];
-
 export function CreateAvatarColor(email: string) {
   let hash = 0;
   for (let i = 0; i < email.length; i++) {
