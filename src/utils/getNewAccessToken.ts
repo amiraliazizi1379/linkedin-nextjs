1d19effcfae639e66a863e0023fc9820af4f2e8e
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-10 22:38:26 +0330

    headers added to arguments

--- src/utils/getNewAccessToken.ts
+++ src/utils/getNewAccessToken.ts
@@ -1,21 +1,19 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function GetNewAccessToken(
   url: string,
-  options: { method: string , headers? : {} , body?: FormData | string }
+  options: { method: string; headers?: {}; body?: FormData | string }
 ) {
   try {
     const res = await fetch(url, {
       method: options.method,
-      headers : options.headers,
+      headers: options.headers,
       body: options.body,
       credentials: "include",
     });
 
     if (res.ok || res.status === 403) return res;
     const result = await res.json();
-   
 
     if (res.status === 401 && result.message === "Token expired") {
       const requestToken = await fetch("http://localhost:3000/api/refresh", {
@@ -25,20 +23,20 @@ export async function GetNewAccessToken(
         },
         credentials: "include",
       });
+
       if (!requestToken.ok) redirect("/login");
 
       if (requestToken.ok) {
         const res2 = await fetch(url, {
           method: options.method,
-          headers : options.headers,
+          headers: options.headers,
           body: options.body,
           credentials: "include",
         });
         if (res2.ok || res2.status === 403) return res2;
       }
-    }
-    else{
-      redirect('/login')
+    } else {
+      redirect("/login");
     }
   } catch (err) {
     throw err;
