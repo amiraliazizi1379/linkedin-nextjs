1fb71c21509d8294ad49042a2b5657b01786ca9e
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-10 22:36:23 +0330

    function edited

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,5 +1,4 @@
 import * as bcrypt from "bcrypt";
-import { AppError } from "./AppError";
 
 export async function hashPassword(password: string): Promise<string> {
   return await bcrypt.hash(password, 10);
@@ -9,7 +8,5 @@ export async function is_validPassword(
   password: string,
   dbPassword: string
 ): Promise<boolean> {
-  const validPassword = await bcrypt.compare(password, dbPassword);
-  if (!validPassword) throw new AppError("invalid Email or Password", 400);
-  return validPassword;
+  return await bcrypt.compare(password, dbPassword);
 }
