6ffb6d4e825995f5b9c6d92ab32cd9606307eb1b
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-11 15:03:10 +0330

    some styles are improved

--- src/app/profile/features/renderposts/components/largPreviewImg.tsx
+++ src/app/profile/features/renderposts/components/largPreviewImg.tsx
@@ -1,4 +1,4 @@
-import { RootState, setLargImg } from "@/app/redux/store";
+import { RootState, setLargImg } from "@/redux/store";
 import { CloseButton } from "../../components/closeButton";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -7,14 +7,14 @@ export function LargPreviewImg() {
   const { fullScreenImgSrc } = useSelector((state: RootState) => state.app);
 
   return (
-    <div className="flex-center">
+    <div className="flex-center fixed">
       <div
         className="fixed inset-0 bg-black opacity-50"
         onClick={() => dispatch(setLargImg(false))}
       ></div>
-      <section className="fixed top-[3rem] flex flex-col items-end max-w-[70vw] h-[90vh] rounded-lg p-4 overflow-auto  bg-[#fff]">
+      <section className="fixed top-[1rem] flex flex-col items-end max-w-[70vw] h-[95vh] rounded-lg p-4   bg-[#fff]">
         <CloseButton setCustomState={setLargImg} />
-        <img src={fullScreenImgSrc} alt="" className="rounded-md" />
+        <img src={fullScreenImgSrc} alt="" className="rounded-md h-[82vh]" />
       </section>
     </div>
   );
