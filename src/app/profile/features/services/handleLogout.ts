cd2bbd5be136740574d1dd11a3df1a1c5c89e48b
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-11 14:59:15 +0330

    loading dispatch added

--- src/app/profile/features/services/handleLogout.ts
+++ src/app/profile/features/services/handleLogout.ts
@@ -1,10 +1,13 @@
+import { setLoading, store } from "@/redux/store";
 import { redirect } from "next/navigation";
 
 export async function handlelogout() {
+  store.dispatch(setLoading(true));
   const res = await fetch("/api/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
   });
   if (res.ok) redirect("/login");
+  store.dispatch(setLoading(false));
 }
