76ac1c87fdbe65aff8b14bd67141a36f0e317600
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-18 23:54:59 +0330

    redux states are replaced.component is completed

--- src/app/profile/features/components/pop-up-user-nav.tsx
+++ src/app/profile/features/components/pop-up-user-nav.tsx
@@ -1,24 +1,39 @@
-'use client'
-
-import { useRouter } from "next/navigation";
+import { useDispatch, useSelector } from "react-redux";
+import { handlelogout } from "../services/handleLogout";
 import UserImageComponent from "./userImgComponent";
+import { RootState, setEditProfile } from "@/redux/store";
 
 export default function PopOp() {
-  const router = useRouter();
-  async function handlelogout() {
-    const res = await fetch("/api/logout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
-    });
-    if (res.ok) router.push("/login");
-  }
-
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state: RootState) => state.app);
+  const { name, image, email, bio } = userData;
   return (
-    <section className="w-[250px] p-2 shadow-md right-[11rem] top-[4.5rem] rounded-md fixed z-20 h-[340px] bg-[#fff]">
-      <UserImageComponent style="w-[50px] h-[50px] text-xl"/>
-      <button className="border-1 border-[#0a66c2] cursor-pointer hover:border-2 hover:bg-[#eaf4fd] h-[1.5rem] w-full mt-2 text-[#0a66c2] text-[13px] font-semibold rounded-full">View Profile</button>
-      <button className="cursor-pointer text-left text-[14px] mt-2 w-full py-2 hover:underline border-t-1 border-gray-300" onClick={handlelogout}>Sign Out</button>
+    <section className="min-w-[250px] p-2 shadow-md right-[11rem] top-[4.5rem] rounded-md fixed z-20 min-h-[150px] bg-[#fff]">
+      <div className="flex gap-4">
+        <UserImageComponent
+          style="w-[50px] h-[50px] text-xl"
+          name={name}
+          image={image}
+          email={email}
+          dontShowLarg
+        />
+        <div>
+          <h3 className="font-semibold">{name ? name : email}</h3>
+          <p className="text-[12px]">{bio}</p>
+        </div>
+      </div>
+      <button
+        onClick={() => dispatch(setEditProfile(true))}
+        className="border-1 border-[#0a66c2] cursor-pointer hover:border-2 hover:bg-[#eaf4fd] h-[1.5rem] w-full mt-2 text-[#0a66c2] text-[13px] font-semibold rounded-full"
+      >
+        View Profile
+      </button>
+      <button
+        className="cursor-pointer text-left text-[14px] mt-2 w-full py-2 hover:underline border-t-1 border-gray-300"
+        onClick={handlelogout}
+      >
+        Sign Out
+      </button>
     </section>
   );
 }
