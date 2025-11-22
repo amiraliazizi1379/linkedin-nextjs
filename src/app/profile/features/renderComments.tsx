e6e0abb077094f33c0ea9947a90121a8c8c3308c
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-22 22:51:46 +0330

    redux states are added

--- src/app/profile/features/renderComments.tsx
+++ src/app/profile/features/renderComments.tsx
@@ -1,24 +1,19 @@
 import { commentDataType } from "@/types/commentDataType";
 import UserImageComponent from "./components/userImgComponent";
+import { useDispatch } from "react-redux";
+import { setFullScreenSrc, setLargImg } from "@/redux/store";
 
 export function RenderComments({
   commentData,
 }: {
   commentData: commentDataType[];
 }) {
+  const dispatch = useDispatch();
   return (
     <section className="mt-8">
       {commentData &&
         commentData.map((post) => {
-          const {
-            comment_id,
-            content,
-            image_url,
-            user_id,
-            name,
-            image,
-            email,
-          } = post;
+          const { comment_id, content, image_url, name, image, email } = post;
           return (
             <div key={comment_id} className="ml-5.5 mt-4 pb-4">
               <article className="flex gap-4 ">
@@ -33,9 +28,13 @@ export function RenderComments({
               <p className="text-md mt-2 ml-10"> {content}</p>
               {image_url && (
                 <img
+                  onClick={() => {
+                    dispatch(setLargImg(true));
+                    dispatch(setFullScreenSrc(image_url));
+                  }}
                   src={image_url}
                   alt=""
-                  className="w-[50%] h-[150px] mt-2"
+                  className="w-[50%] h-[150px] mt-2 cursor-pointer"
                 />
               )}
             </div>
