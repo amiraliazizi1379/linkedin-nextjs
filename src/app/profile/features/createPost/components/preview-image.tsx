f30db170d26825d33803c8cdc46e82e90b619960
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-22 22:47:49 +0330

    redux states are added.

--- src/app/profile/features/createPost/components/preview-image.tsx
+++ src/app/profile/features/createPost/components/preview-image.tsx
@@ -1,31 +1,32 @@
-import { Dispatch, SetStateAction, useCallback, useRef } from "react";
+import { RootState, setCommentImgSrc, setpostImgSrc } from "@/redux/store";
 import { IoMdClose } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
 
 type props = {
-  srcImg: string;
-  setSrcImg: Dispatch<SetStateAction<string>>;
   classname?: string;
 };
 
-export function PostImageUploader({
-  srcImg,
-  setSrcImg,
-  classname = "w-full",
-}: props) {
+export function PostImageUploader({ classname = "w-full" }: props) {
+  const dispatch = useDispatch();
+  const { commentImgSrc, postImgSrc } = useSelector(
+    (state: RootState) => state.app
+  );
   return (
-    <main className={`h-[65%] overflow-auto ${classname}`}>
+    <main className={`max-h-[200px] overflow-auto ${classname}`}>
       <div className="h-full">
-        {srcImg && (
+        {(commentImgSrc || postImgSrc) && (
           <section>
             <button
-              onClick={() => setSrcImg("")}
+              onClick={() => {
+                dispatch(setpostImgSrc("")), dispatch(setCommentImgSrc(""));
+              }}
               className={`text-2xl mb-3 h-[32px] flex justify-self-end text-[#fff] cursor-pointer bg-[#4b4b4b] rounded-full hover:bg-[#252525] p-1 ${
                 classname && "absolute top-3 right-4 opacity-80"
               }`}
             >
               <IoMdClose />
             </button>
-            <img src={srcImg} />
+            <img src={commentImgSrc || postImgSrc} />
           </section>
         )}
       </div>
