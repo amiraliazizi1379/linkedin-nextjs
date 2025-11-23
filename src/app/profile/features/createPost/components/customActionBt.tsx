095177d4ade37be2663c24420b1e91c4a555c95a
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-23 12:52:23 +0330

    redux path is changed.btn loading is added.

--- src/app/profile/features/createPost/components/customActionBt.tsx
+++ src/app/profile/features/createPost/components/customActionBt.tsx
@@ -1,4 +1,4 @@
-import { RootState } from "@/app/redux/store";
+import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
 import { ClipLoader } from "react-spinners";
 
@@ -6,7 +6,6 @@ type props = {
   classname?: string;
   name?: string;
   ImgSrc: string;
-  text?: string;
   onclick: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void>;
 };
 
@@ -14,10 +13,9 @@ export function CustomActionBtn({
   classname,
   name = "Post",
   ImgSrc,
-  text,
   onclick,
 }: props) {
-  const { loading } = useSelector((state: RootState) => state.app);
+  const { btnLoading, postBt } = useSelector((state: RootState) => state.app);
   return (
     <div
       className={`flex items-center ${classname} gap-4 justify-end border-t-1 border-gray-300 w-full pt-4 ${
@@ -27,12 +25,12 @@ export function CustomActionBtn({
       <button
         onClick={(e) => onclick(e)}
         className={`${
-          text || ImgSrc
+          postBt || ImgSrc
             ? "bg-[#0a66c2] text-[#fff] hover:bg-[#0f467c] cursor-pointer"
             : "bg-gray-200 text-gray-400  cursor-not-allowed"
         } rounded-full   py-1 px-4  text-sm font-semibold flex-center`}
       >
-        {loading ? <ClipLoader color="#fff" size={17} /> : name}
+        {btnLoading ? <ClipLoader color="#fff" size={17} /> : name}
       </button>
     </div>
   );
