5b0384b0e8f136e30fb027c2f9906d65e7066807
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-23 14:43:20 +0330

    custom handler is created for post , comment and edit profile services.

---
+++ src/app/profile/features/services/customSendPhotoHandler.ts
@@ -0,0 +1,52 @@
+import { setBtnLoading, setEditEmailStatus, store } from "@/redux/store";
+import { GetNewAccessToken } from "@/utils/getNewAccessToken";
+import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
+
+export async function CustomHandler(
+  e: React.MouseEvent<HTMLButtonElement>,
+  postImageFile: File | null,
+  setState: ActionCreatorWithPayload<boolean>,
+  route: string
+) {
+  const {
+    userData,
+    postText,
+    commentText,
+    editedBio,
+    editedEmail,
+    editedUserName,
+    postBt,
+  } = store.getState().app;
+  const { bio, name, email } = userData;
+
+  if (postBt || postImageFile) {
+    store.dispatch(setBtnLoading(true));
+    e.preventDefault();
+    const formData = new FormData();
+    if (postImageFile) formData.append("img", postImageFile);
+    if (postText) formData.append("posttext", postText);
+    if (commentText) formData.append("commenttext", commentText);
+    if (editedBio !== bio) formData.append("bio", editedBio);
+    if (editedEmail !== email) formData.append("email", editedEmail);
+    if (editedUserName !== name) formData.append("name", editedUserName);
+
+    try {
+      const res = await GetNewAccessToken(`/api/${route}`, {
+        method: "POST",
+        body: formData,
+      });
+      if (res?.status === 403) {
+        store.dispatch(setBtnLoading(false));
+        store.dispatch(setEditEmailStatus(true));
+      }
+
+      if (res?.ok) {
+        store.dispatch(setEditEmailStatus(false));
+        store.dispatch(setState(false));
+        store.dispatch(setBtnLoading(false));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
