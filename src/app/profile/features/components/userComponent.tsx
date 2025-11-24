5ab3e4680a5ad95cafaf2a7fc9c20f9095823bb1
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-24 12:34:54 +0330

    user component is created for home page

---
+++ src/app/profile/features/components/userComponent.tsx
@@ -0,0 +1,20 @@
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+import UserImageComponent from "./userImgComponent";
+
+export function UserComponent() {
+  const { userData } = useSelector((state: RootState) => state.app);
+  const { id, email, name, bio, image } = userData;
+  return (
+    <div className="fixed top-22 left-44 w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-200 rounded-md">
+      <UserImageComponent
+        style="w-[80px] h-[80px] mx-auto mt-2"
+        email={email}
+        image={image}
+        name={name}
+      />
+      <h1 className="font-semibold mt-4">{name ? name : email}</h1>
+      <p>{bio && bio}</p>
+    </div>
+  );
+}
