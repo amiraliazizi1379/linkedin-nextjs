44c89bec98672eda19111d2dd68b29182ce356c8
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-27 15:09:43 +0330

    port is added to config

--- src/app/api/dbConnection/db.ts
+++ src/app/api/dbConnection/db.ts
@@ -1,10 +1,11 @@
 import { createPool } from "mysql2";
 
 export const pool = createPool({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
   uri: process.env.MYSQL_URL,
   waitForConnections: true,
   connectionLimit: 10,
