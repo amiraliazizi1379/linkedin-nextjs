09369438dd172afc1d53c84c75391b58d07db1a5
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-28 23:07:26 +0330

    fixed

--- src/app/api/testdbconnection/route.ts
+++ src/app/api/testdbconnection/route.ts
@@ -4,9 +4,9 @@ import { pool } from "../dbConnection/db";
 export const GET = async () => {
   try {
     const result = await pool.query("SELECT NOW()");
-    NextResponse.json({ success: true, time: result.rows[0] });
+   return NextResponse.json({ success: true, time: result.rows[0] });
   } catch (error) {
     console.error("DB Connection Error:", error);
-    NextResponse.json({ success: false, error: error });
+    return NextResponse.json({ success: false, error: error });
   }
 };
