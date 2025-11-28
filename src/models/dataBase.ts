f211680ca1ad862dc689f2181b93b224efab216b
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-29 00:43:59 +0330

    question marks are changed to dollar sign

--- src/models/dataBase.ts
+++ src/models/dataBase.ts
@@ -12,7 +12,7 @@ export class databaseOperation {
   };
   static addUser = async (email: string, password: string) => {
     const result = await pool.query(
-      "insert into users (email , password ) values  ( ? , ? ) RETURNING id;",
+      "insert into users (email , password ) values  ( $1 , $2 ) RETURNING id ;",
       [email, password]
     );
 
@@ -20,59 +20,59 @@ export class databaseOperation {
   };
 
   static addProfileImage = async (image_url: string | null, id: number) => {
-    return await pool.query("update users set  image = ? where id = ?;", [
+    return await pool.query("update users set  image = $1 where id = $2 ;", [
       image_url,
       id,
     ]);
   };
   static deleteProfileImage = async (id: number) => {
-    return await pool.query("update users set image = null where id = ?;", [
+    return await pool.query("update users set image = null where id = $ ;", [
       id,
     ]);
   };
 
   static editEmail = async (email: string | null, id: number) => {
-    return await pool.query("update users set  email = ? where id = ?;", [
+    return await pool.query("update users set  email = $1 where id = $2 ;", [
       email,
       id,
     ]);
   };
 
   static addBio = async (bio: string | null, id: number) => {
-    return await pool.query("update users set  bio = ? where id = ?;", [
+    return await pool.query("update users set  bio = $1 where id = $2 ;", [
       bio,
       id,
     ]);
   };
   static editName = async (name: string, id: number) => {
-    return await pool.query("update users set  name = ?  where id = ?;", [
+    return await pool.query("update users set  name = $1  where id = $2 ;", [
       name,
       id,
     ]);
   };
 
   static findToken = async (id: number) => {
-    const user = await pool.query("select * from users  where id = ?;", [id]);
+    const user = await pool.query("select * from users  where id = $ ;", [id]);
     return user.rows[0];
   };
 
   static deleteToken = async (userId: number) => {
     return await pool.query(
-      "update users set refreshtoken = null where id = ?;",
+      "update users set refreshtoken = null where id = $;",
       [userId]
     );
   };
 
   static updateToken = async (token: string, id: number) => {
-    return await pool.query("update users set refreshtoken = ? where id = ?;", [
-      token,
-      id,
-    ]);
+    return await pool.query(
+      "update users set refreshtoken = $1 where id = $2 ;",
+      [token, id]
+    );
   };
 
   static getUserData = async (id: number) => {
     const userDat = await pool.query<RowDataPacket[]>(
-      "select id , name , email , image , bio from users where id = ?;",
+      "select id , name , email , image , bio from users where id = $ ;",
       [id]
     );
     return userDat.rows;
@@ -80,7 +80,7 @@ export class databaseOperation {
 
   static getAllUsers = async (id: number) => {
     const userDat = await pool.query(
-      "select id , name , email , image , bio  , CASE  WHEN EXISTS ( SELECT 1  FROM follows WHERE follows.follower_id = ? AND follows.followed_id = users.id) THEN TRUE ELSE FALSE  END AS is_following from users where id != ?;",
+      "select id , name , email , image , bio  , CASE  WHEN EXISTS ( SELECT 1  FROM follows WHERE follows.follower_id = $ AND follows.followed_id = users.id) THEN TRUE ELSE FALSE  END AS is_following from users where id != $ ;",
       [id, id]
     );
     return userDat.rows;
@@ -88,7 +88,7 @@ export class databaseOperation {
 
   static addPost = async (userId: number, text: string, img: string | null) => {
     const addPostQuery = await pool.query<ResultSetHeader>(
-      "insert into posts (user_id , content , image_url) values (? , ? , ?);",
+      "insert into posts (user_id , content , image_url) values ($1 , $2 , $3);",
       [userId, text, img]
     );
     return addPostQuery;
@@ -101,7 +101,7 @@ export class databaseOperation {
     img: string | null
   ) => {
     const addCommentQuery = await pool.query<ResultSetHeader>(
-      "insert into comments (user_id , post_id , content , image_url) values (? , ? , ? , ?);",
+      "insert into comments (user_id , post_id , content , image_url) values ($1 , $2 , $3 , $4) ;",
       [userId, postId, text, img]
     );
     return addCommentQuery;
@@ -109,7 +109,7 @@ export class databaseOperation {
 
   static getPostsData = async (id: number) => {
     const postsDat = await pool.query(
-      "SELECT posts.id AS post_id,posts.image_url,posts.content,posts.created_at,users.id AS user_id,users.image,users.name,users.email,users.bio,CASE WHEN EXISTS ( SELECT 1 FROM likes  WHERE likes.user_id = ? AND likes.post_id = posts.id) THEN TRUE ELSE FALSE END AS liked,CASE  WHEN EXISTS ( SELECT 1  FROM follows WHERE follows.follower_id = ? AND follows.followed_id = users.id) THEN TRUE ELSE FALSE  END AS is_following,COALESCE(like_counts.like_count, 0) AS like_count,COALESCE(comment_counts.comment_count, 0) AS comment_count FROM posts JOIN users ON posts.user_id = users.id LEFT JOIN (SELECT post_id, COUNT(*) AS like_count FROM likes GROUP BY post_id) AS like_counts ON like_counts.post_id = posts.id LEFT JOIN (SELECT post_id, COUNT(*) AS comment_count FROM comments GROUP BY post_id) AS comment_counts ON comment_counts.post_id = posts.id ORDER BY posts.created_at DESC limit 5;",
+      "SELECT posts.id AS post_id,posts.image_url,posts.content,posts.created_at,users.id AS user_id,users.image,users.name,users.email,users.bio,CASE WHEN EXISTS ( SELECT 1 FROM likes  WHERE likes.user_id = $ AND likes.post_id = posts.id) THEN TRUE ELSE FALSE END AS liked,CASE  WHEN EXISTS ( SELECT 1  FROM follows WHERE follows.follower_id = $ AND follows.followed_id = users.id) THEN TRUE ELSE FALSE  END AS is_following,COALESCE(like_counts.like_count, 0) AS like_count,COALESCE(comment_counts.comment_count, 0) AS comment_count FROM posts JOIN users ON posts.user_id = users.id LEFT JOIN (SELECT post_id, COUNT(*) AS like_count FROM likes GROUP BY post_id) AS like_counts ON like_counts.post_id = posts.id LEFT JOIN (SELECT post_id, COUNT(*) AS comment_count FROM comments GROUP BY post_id) AS comment_counts ON comment_counts.post_id = posts.id ORDER BY posts.created_at DESC limit 5;",
       [id, id]
     );
     return postsDat.rows;
@@ -117,7 +117,7 @@ export class databaseOperation {
 
   static getComments = async (id: number) => {
     const postsComments = await pool.query(
-      "select c.id AS comment_id,c.post_id,c.user_id, c.content,c.image_url,u.image, u.name,u.email from comments c join  users u ON u.id = c.user_id where c.post_id = ? ;",
+      "select c.id AS comment_id,c.post_id,c.user_id, c.content,c.image_url,u.image, u.name,u.email from comments c join  users u ON u.id = c.user_id where c.post_id = $ ;",
       [id]
     );
     return postsComments;
@@ -125,28 +125,28 @@ export class databaseOperation {
 
   static registerLike = async (userId: number, postId: number) => {
     const likeResult = await pool.query(
-      "insert into likes (user_id , post_id) values (? , ?);",
+      "insert into likes (user_id , post_id) values ($1 , $2) ;",
       [userId, postId]
     );
     return likeResult;
   };
   static deleteLike = async (userId: number, postId: number) => {
     const likeResult = await pool.query(
-      "DELETE from likes where user_id = ?  AND  post_id = ?;",
+      "DELETE from likes where user_id = $1  AND  post_id = $2 ;",
       [userId, postId]
     );
     return likeResult;
   };
   static registerFollow = async (userId: number, postId: number) => {
     const likeResult = await pool.query(
-      "insert into follows (follower_id , followed_id) values (? , ?);",
+      "insert into follows (follower_id , followed_id) values ($1 , $2);",
       [userId, postId]
     );
     return likeResult;
   };
   static deleteFollow = async (userId: number, postId: number) => {
     const likeResult = await pool.query(
-      "DELETE from follows where follower_id = ?  AND  followed_id = ?;",
+      "DELETE from follows where follower_id = $1  AND  followed_id = $2;",
       [userId, postId]
     );
     return likeResult;
