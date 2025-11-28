15b0cb023ccdcac9c278626428769e8f657bf6fa
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-28 23:01:50 +0330

    next response is replaced

--- src/app/api/testdbconnection/route.ts
+++ src/app/api/testdbconnection/route.ts
@@ -1,11 +1,12 @@
+import { NextResponse } from "next/server";
 import { pool } from "../dbConnection/db";
 
 export const GET = async () => {
   try {
     const result = await pool.query("SELECT NOW()");
-    Response.json({ success: true, time: result.rows[0] });
+    NextResponse.json({ success: true, time: result.rows[0] });
   } catch (error) {
     console.error("DB Connection Error:", error);
-    Response.json({ success: false, error: error });
+    NextResponse.json({ success: false, error: error });
   }
 };
