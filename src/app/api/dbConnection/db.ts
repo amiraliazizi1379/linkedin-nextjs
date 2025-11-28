a217bd68f0fc39ae1e9af79c5734083541c230f7
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-28 22:05:30 +0330

    pool connection is updated for postgres

--- src/app/api/dbConnection/db.ts
+++ src/app/api/dbConnection/db.ts
@@ -1,12 +1,21 @@
-import { createPool } from "mysql2";
+import { Pool } from "pg";
 
-export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT),
-  uri: process.env.MYSQL_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-}).promise();
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// import { createPool } from "mysql2";
+
+// export const pool = createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: Number(process.env.DB_PORT),
+//   uri: process.env.MYSQL_URL,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// }).promise();
