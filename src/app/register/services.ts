dd87f64874ca6d49d2108d673b87949b4fae89e0
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-29 18:34:11 +0330

    test

--- src/app/register/services.ts
+++ src/app/register/services.ts
@@ -8,7 +8,7 @@ export async function RegisterOnSubmit(
   setError: UseFormSetError<loginType>
 ) {
   try {
-    const res = await fetch(`${process.env.DOMAIN}/api/register`, {
+    const res = await fetch(`/api/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
