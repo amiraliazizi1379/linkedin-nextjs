2821e3e0a4e8531ac2838ec7cac8c3cf6951f417
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-11-30 14:48:21 +0330

    z index is edited

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -7,7 +7,7 @@ export function UserComponent() {
   const { id, email, name, bio, image } = userData;
   console.log(userData)
   return (
-    <div className="w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-200 rounded-md">
+    <div className="w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-300 rounded-md">
       <UserImageComponent
         style="w-[80px] h-[80px] mx-auto mt-2"
         email={email}
