9ad856278f2dcdd6253a326237d5252331d5077a
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-01 13:09:26 +0330

    new domain for images added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com" , "googleusercontent.com"],
   },
 };
 
