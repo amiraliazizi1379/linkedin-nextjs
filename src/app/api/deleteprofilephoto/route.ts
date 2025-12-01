a614a596622284d09615a88c7138d4d8cc6c1e0e
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-01 15:53:29 +0330

    data structuring is eidted

--- src/app/api/deleteprofilephoto/route.ts
+++ src/app/api/deleteprofilephoto/route.ts
@@ -4,9 +4,9 @@ import { databaseOperation } from "@/models/dataBase";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = auth(async (req: NextRequest, userId) => {
-  const image_url = await databaseOperation.getImageUrl(userId);
-  console.log(image_url)
-  const public_id = image_url.split("/upload/")[1].split(".")[0];
+  const { image } = await databaseOperation.getImageUrl(userId);
+
+  const public_id = image.split("/upload/")[1].split(".")[0];
   const result = await cloudinary.uploader.destroy(public_id);
   if (!result)
     return NextResponse.json(
