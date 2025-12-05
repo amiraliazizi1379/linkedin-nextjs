9caf699202a9e2f753b783f967746eb79359e774
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 01:10:39 +0330

    next updated to 16.0.7

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -7,7 +7,7 @@ export function UserComponent() {
   const { id, email, name, bio, image } = userData;
   console.log(userData);
   return (
-    <div className="w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-300 rounded-md">
+    <div className="w-[250px] h-[220px] bg-[#fff] border-1 border-gray-300 rounded-md">
       <div className="flex h-[70px]">
         <div className="w-[70%] bg-gray-200">
           <div className="w-[90%] bg-gray-400 rounded-full h-full"></div>
