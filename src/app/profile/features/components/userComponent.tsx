6607db7dc3b4d5827c57fd4525ef0de8bf793808
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 00:45:47 +0330

    back ground for user component is created

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -8,6 +8,11 @@ export function UserComponent() {
   console.log(userData)
   return (
     <div className="w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-300 rounded-md">
+      <div className="flex">
+        <div className="w-[60%] bg-gray-600 rounded-full"></div>
+        <div className="w-[10%] bg-gray-200"></div>
+        <div className="w-[30%] bg-gray-400"></div>
+      </div>
       <UserImageComponent
         style="w-[80px] h-[80px] mx-auto mt-2"
         email={email}
