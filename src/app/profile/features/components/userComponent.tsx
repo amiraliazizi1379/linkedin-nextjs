6d9154ff57bf93443da4f509117a6595ee55ce1c
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 00:58:37 +0330

    add styles

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -5,13 +5,14 @@ import UserImageComponent from "./userImgComponent";
 export function UserComponent() {
   const { userData } = useSelector((state: RootState) => state.app);
   const { id, email, name, bio, image } = userData;
-  console.log(userData)
+  console.log(userData);
   return (
     <div className="w-[250px] h-[220px] bg-[#fff] p-4  border-1 border-gray-300 rounded-md">
-      <div className="flex h-[50px]">
-        <div className="w-[60%] bg-gray-600 rounded-full"></div>
-        <div className="w-[10%] bg-gray-200"></div>
-        <div className="w-[30%] bg-gray-400"></div>
+      <div className="flex h-[70px]">
+        <div className="w-[70%] bg-gray-200">
+          <div className="w-[90%] bg-gray-400 rounded-full"></div>
+        </div>
+        <div className="w-[30%] bg-gray-300"></div>
       </div>
       <UserImageComponent
         style="w-[80px] h-[80px] mx-auto mt-2"
