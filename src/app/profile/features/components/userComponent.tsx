6e4c8ccc656091472599fe0a00ec0b953bcb5849
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 13:04:21 +0330

    css

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -8,19 +8,19 @@ export function UserComponent() {
   console.log(userData);
   return (
     <div className="w-[250px] h-[220px] bg-[#fff] border-1 border-gray-300 rounded-xl relative">
-      <div className="flex h-[70px]">
+      <div className="flex h-[70px] rounded-t-xl">
         <div className="w-[70%] bg-gray-200">
           <div className="w-[90%] bg-[#b2bec3] rounded-r-4xl h-full"></div>
         </div>
-        <div className="w-[30%] bg-[#b2bec3]"></div>
+        <div className="w-[30%] bg-[#dfe6e9]"></div>
       </div>
       <UserImageComponent
-        style="w-[80px] h-[80px] border-2 border-[#fff] absolute left-4 top-8"
+        style="w-[80px] h-[80px] border-4 border-[#fff] absolute left-4 top-8"
         email={email}
         image={image}
         name={name}
       />
-      <h1 className="font-semibold mt-10">{name ? name : email}</h1>
+      <h1 className="font-semibold mt-12 ml-4">{name ? name : email}</h1>
       <p>{bio && bio}</p>
     </div>
   );
