016ca9aa315b11994590e3b616660852c38a8b22
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 17:30:24 +0330

    user component is completed

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
 import UserImageComponent from "./userImgComponent";
@@ -8,11 +9,11 @@ export function UserComponent() {
   console.log(userData);
   return (
     <div className="w-[250px] h-[220px] bg-[#fff] border-1 border-gray-300 rounded-xl relative">
-      <div className="flex h-[70px] w-full">
-        <div className="w-[70%] bg-gray-200 rounded-tl-xl">
-          <div className="w-[90%] bg-[#bdc0c2] rounded-tl-xl rounded-r-4xl h-full"></div>
+      <div className="flex h-[60px] w-full">
+        <div className="w-[70%] bg-gray-200 rounded-tl-lg">
+          <div className="w-[130%] bg-[#a6bacb] rounded-tl-lg [clip-path:circle(50%_at_33%_50%)] h-full"></div>
         </div>
-        <div className="w-[30%] bg-[#cad7e3] rounded-tr-xl"></div>
+        <div className="w-[30%] bg-[#cad7e3] rounded-tr-lg"></div>
       </div>
       <UserImageComponent
         style="w-[80px] h-[80px] border-3 border-[#fff] absolute left-4 top-9"
