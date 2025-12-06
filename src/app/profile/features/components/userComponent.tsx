3c2f14a48ae0ebd4b777ad937384e8b2921d97dd
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-06 12:53:56 +0330

    hg

--- src/app/profile/features/components/userComponent.tsx
+++ src/app/profile/features/components/userComponent.tsx
@@ -7,15 +7,15 @@ export function UserComponent() {
   const { id, email, name, bio, image } = userData;
   console.log(userData);
   return (
-    <div className="w-[250px] h-[220px] bg-[#fff] border-1 border-gray-300 rounded-md relative">
+    <div className="w-[250px] h-[220px] bg-[#fff] border-1 border-gray-300 rounded-xl relative">
       <div className="flex h-[70px]">
         <div className="w-[70%] bg-gray-200">
-          <div className="w-[90%] bg-[#636e72] rounded-r-3xl h-full"></div>
+          <div className="w-[90%] bg-[#b2bec3] rounded-r-4xl h-full"></div>
         </div>
         <div className="w-[30%] bg-[#b2bec3]"></div>
       </div>
       <UserImageComponent
-        style="w-[80px] h-[80px] mx-auto mt-2 absolute left-1 top-4"
+        style="w-[80px] h-[80px] mx-auto mt-2 border-1 border-[#fff] absolute left-4 top-8"
         email={email}
         image={image}
         name={name}
