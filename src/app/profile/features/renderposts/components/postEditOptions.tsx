41b59635dbfd0c7ebef3c464f464ed95cb815f0c
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-08 12:12:14 +0330

    ri

--- src/app/profile/features/renderposts/components/postEditOptions.tsx
+++ src/app/profile/features/renderposts/components/postEditOptions.tsx
@@ -6,7 +6,7 @@ export function PostEditOptions() {
   const { postEditOptions } = useSelector((state: RootState) => state.app);
   return (
     <div
-      className={`fixed flex flex-col items-start right-[34.5rem] py-1 shadow-md opacity-0 -translate-y-2  rounded-md transition-all duration-200 w-[200px] text-[14px] 
+      className={`fixed flex flex-col items-start right-[30rem] py-1 shadow-md opacity-0 -translate-y-2  rounded-md transition-all duration-200 w-[200px] text-[14px] 
                    
                     ${
                       postEditOptions
