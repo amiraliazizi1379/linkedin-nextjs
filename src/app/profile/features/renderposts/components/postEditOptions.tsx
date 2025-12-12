e4ce357c10025fbb993c8ed98bae3098ca8c79d8
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-12 22:21:09 +0330

    sldkjw

--- src/app/profile/features/renderposts/components/postEditOptions.tsx
+++ src/app/profile/features/renderposts/components/postEditOptions.tsx
@@ -3,7 +3,7 @@ import { MdDelete, MdOutlineModeEditOutline } from "react-icons/md";
 export default function PostEditOptions({ active }: { active: boolean }) {
   return (
     <div
-      className={`absolute flex flex-col items-start  py-1 shadow-md rounded-md transition-all duration-200 w-[230px] text-[14px] 
+      className={`absolute flex flex-col items-start right-[220px]  py-1 shadow-md rounded-md transition-all duration-200 w-[230px] text-[14px] 
                    
                     ${
                       active
