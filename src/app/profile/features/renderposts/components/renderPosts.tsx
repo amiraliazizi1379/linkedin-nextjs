b4ef5ae2bf7c5ebe156a11af912d4a7b02fc8d2f
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-12 19:54:08 +0330

    es

--- src/app/profile/features/renderposts/components/renderPosts.tsx
+++ src/app/profile/features/renderposts/components/renderPosts.tsx
@@ -89,7 +89,7 @@ export default function RenderPosts({ userId }: { userId: string }) {
                   onClick={() => {
                     const updatedPostData = postData.map((post) =>
                       post.post_id === post_id
-                        ? { ...post, activeOptions: !post.activePostOptions }
+                        ? { ...post, activePostOptions: !activePostOptions }
                         : post
                     );
                     dispatch(setPostData(updatedPostData));
