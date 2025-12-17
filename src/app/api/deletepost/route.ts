e1e3701ca9c9a9879f9699d62502e8a795450137
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-17 13:53:56 +0330

    log the id for debuging

--- src/app/api/deletepost/route.ts
+++ src/app/api/deletepost/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = auth(async (req: NextRequest, userId) => {
   const postId = await req.json();
+  console.log(postId)
   const { image } = await databaseOperation.getImageUrl(postId, "posts");
   if (image) {
     const public_id = image.split("/upload/")[1].split(".")[0];
