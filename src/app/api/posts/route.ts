778320079e8d616d7d575aa8860084fbee5400f9
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-27 14:43:24 +0330

    req

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -34,13 +34,13 @@ export const PATCH = auth(
     if (!postId)
       return NextResponse.json({ message: "post not found" }, { status: 400 });
 
-    const  image  = await databaseOperation.getImageUrl(
+    const { image_url } = await databaseOperation.getImageUrl(
       Number(postId),
       "posts"
     );
-    console.log("reqimage : " , imageUrl , "  image in database : " , image)
-    if (!imageUrl && image) {
-      const public_id = image.split("/upload/")[1].split(".")[0];
+    console.log("reqimage : ", imageUrl, "  image in database : ", image_url);
+    if (!imageUrl && image_url) {
+      const public_id = image_url.split("/upload/")[1].split(".")[0];
       const result = await cloudinary.uploader.destroy(public_id);
       if (!result)
         return NextResponse.json(
