4d7593dd26b731b396f8afce7fdede97d5c83f76
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-28 23:32:50 +0330

    gh

--- src/app/profile/features/createPost/components/customActionBt.tsx
+++ src/app/profile/features/createPost/components/customActionBt.tsx
@@ -19,8 +19,7 @@ export function CustomActionBtn({
   return (
     <div
       className={`flex items-center ${classname} gap-4 justify-end border-t-1 border-gray-300 w-full pt-4 ${
-        ((ImgSrc && name !== "Delete") || (!ImgSrc && name === "Save")) &&
-        "mt-8"
+        ((ImgSrc && name !== "Delete") || name === "Save") && "mt-8"
       }`}
     >
       <button
