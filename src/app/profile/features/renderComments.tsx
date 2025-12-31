69e47efa4166ccdbc9ff906dcc7f46c26bfef3f8
amiraliazizi1379 <amir7913azizi@gmail.com>
2025-12-31 14:28:13 +0330

    ert

--- src/app/profile/features/renderComments.tsx
+++ src/app/profile/features/renderComments.tsx
@@ -51,12 +51,14 @@ export function RenderComments({
                     <p className="text-[14px] text-gray-400">{bio}</p>
                   </div>
                 </article>
-                {Number(userId) === user_id && (
-                  <ThreeDotsOptions
-                    post_id={post_id}
-                    activePostOptions={activeCommentOption}
-                  />
-                )}
+                <div className="mr-4">
+                  {Number(userId) === user_id && (
+                    <ThreeDotsOptions
+                      post_id={post_id}
+                      activePostOptions={activeCommentOption}
+                    />
+                  )}
+                </div>
               </div>
               <p className="p-4">
                 {readMore ? content : content.substring(0, 100)}
